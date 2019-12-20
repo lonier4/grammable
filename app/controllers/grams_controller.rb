5ff4064d6d62c8a6294c62ea80112f56d90d50a5
Andre Yeargins <lonier4@gmail.com>
2019-12-20 03:53:01 +0000

    added devise and added column to gram table to connect users and grams table, troubleshooting rspec test for grams nil

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,15 +1,16 @@
 class GramsController < ApplicationController
-  def index
-    
-  end
+  before_action :authenticate_user!, only: [:new, :ceate]
 
   def new
     @gram = Gram.new
   end
 
+  def index
+  end
+
   def create
-    @gram = Gram.create(gram_params)
-    if @gram.vaild?
+    @gram = current_user.grams.create(gram_params)
+    if @gram.valid?
       redirect_to root_path
     else
       render :new, status: :unprocessable_entity
