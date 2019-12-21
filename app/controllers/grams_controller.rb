ce282db1e95ff7f8c16c65430c676ea21fd09646
Andre Yeargins <lonier4@gmail.com>
2019-12-21 23:26:28 +0000

    troubleshooting carrierwave

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,11 +1,12 @@
 class GramsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :ceate]
+  before_action :authenticate_user!, only: [:new, :ceate, :edit, :update, :destroy]
 
   def new
     @gram = Gram.new
   end
 
   def index
+    @grams = Gram.all
   end
 
   def create
@@ -17,10 +18,48 @@ class GramsController < ApplicationController
     end
   end
 
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+  end
+  
+
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
+  end
+  
+
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
+
+    @gram.update_attributes(gram_params)
+    if @gram.valid?
+      redirect_to root_path  
+    else
+      return render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
+
+    @gram.destroy
+    redirect_to root_path
+  end
+
   private
 
   def gram_params
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
 
+  def render_not_found(status=:not_found)
+    render plain: "#{status.to_s.titleize} :(", status: status
+  end
 end
